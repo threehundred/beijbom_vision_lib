82db96d38628cde8db0ffeac042830bdd9253f7e
Oscar Beijbom <oscar.beijbom@gmail.com>
2015-12-05 23:31:21 -0800

    bugfix

--- beijbom_caffe_data_layers.py
+++ beijbom_caffe_data_layers.py
@@ -45,7 +45,7 @@ class RandomPointDataLayer(caffe.Layer):
         # === Check some of the input variables
         imlist = [line.rstrip('\n') for line in open(params['imlistfile'])]
         assert len(imlist) >= params['imgs_per_batch'], 'Image list must be longer than the number of images you ask for per batch.'
-        assert scaling_method in ('ratio', 'scale')
+        assert params['scaling_method'] in ('ratio', 'scale')
 
         # === set up thread and batch advancer ===
         self.thread_result = {}
