679b79c8813b14596e8e826d87c420df959c2c22
Oscar Beijbom <oscar.beijbom@gmail.com>
2015-12-05 23:11:33 -0800

    added random offset as data augmentation

--- beijbom_caffe_data_layers.py
+++ beijbom_caffe_data_layers.py
@@ -38,35 +38,22 @@ class RandomPointDataLayer(caffe.Layer):
         assert 'scaling_method' in params.keys(), 'Params must include scaling_method'
         assert 'scaling_factor' in params.keys(), 'Params must include scaling_factor'
         assert 'im_mean' in params.keys(), 'Params must include im_mean.'
-
-        self.t0 = 0
-        self.t1 = 0
+        assert 'rand_offset' in params.keys(), 'Params must include rand_offset.'
         
         self.batch_size = params['batch_size']
-        imlist = [line.rstrip('\n') for line in open(params['imlistfile'])]
-        with open(params['imdictfile']) as f:
-            imdict = json.load(f)
-        imgs_per_batch = params['imgs_per_batch']
-        crop_size = params['crop_size']
-        scaling_method = params['scaling_method']
-        scaling_factor = params['scaling_factor']
-
-        transformer = TransformerWrapper(params['im_mean'])
 
         # === Check some of the input variables
         assert len(imlist) >= imgs_per_batch, 'Image list must be longer than the number of images you ask for per batch.'
         assert scaling_method in ('ratio', 'scale')
 
-        print "Setting up RandomPointDataLayer with batch size:{}".format(self.batch_size)
-
         # === set up thread and batch advancer ===
         self.thread_result = {}
         self.thread = None
-        self.batch_advancer = PatchBatchAdvancer(self.thread_result, self.batch_size, imlist, imdict, imgs_per_batch, crop_size, transformer, scaling_method, scaling_factor)
+        self.batch_advancer = PatchBatchAdvancer(self.thread_result, params)
         self.dispatch_worker()
 
         # === reshape tops ===
-        top[0].reshape(self.batch_size, 3, crop_size, crop_size)
+        top[0].reshape(self.batch_size, 3, params['crop_size'], params['crop_size'])
         top[1].reshape(self.batch_size, 1)
 
     def reshape(self, bottom, top):
@@ -76,15 +63,11 @@ class RandomPointDataLayer(caffe.Layer):
     def forward(self, bottom, top):
         #print time.clock() - self.t0, "seconds since last call to forward."
         if self.thread is not None:
-            self.t1 = timer()
             self.join_worker()
-            #print "Waited ", timer() - self.t1, "seconds for join."
 
         for top_index, name in zip(range(len(top)), self.top_names):
             for i in range(self.batch_size):
-                #rint i, top_index, name
                 top[top_index].data[i, ...] = self.thread_result[name][i] 
-        self.t0 = time.clock()
         self.dispatch_worker()
 
     def dispatch_worker(self):
@@ -107,38 +90,35 @@ class PatchBatchAdvancer():
     """
     The PatchBatchAdvancer is a helper class to RandomPointDataLayer. It is called asychronosly and prepares the tops.
     """
-    def __init__(self, result, batch_size, imlist, imdict, imgs_per_batch, crop_size, transformer, scaling_method, scaling_factor):
+    def __init__(self, result, params):
         self._cur = 0
         self.result = result
-        self.batch_size = batch_size
-        self.imlist = imlist
-        self.imdict = imdict
-        self.imgs_per_batch = imgs_per_batch
-        self.crop_size = crop_size
-        self.transformer = transformer
-        self.scaling_method = scaling_method
-        self.scaling_factor = scaling_factor
+        self.params = params
+        self.imlist = [line.rstrip('\n') for line in open(params['imlistfile'])]
+        with open(params['imdictfile']) as f:
+            self.imdict = json.load(f)
+        self.transformer = TransformerWrapper(params['im_mean'])
         shuffle(self.imlist)
 
-        print "PatchBatchAdvancer is initialized with {} images, {} imgs per batch, and {}x{} pixel patches".format(len(imlist), imgs_per_batch, crop_size, crop_size)
+        print "DataLayer initialized with {} images, {} imgs per batch, and {}x{} pixel patches".format(len(self.imlist), params['imgs_per_batch'], params['crop_size'], params['crop_size'])
 
     def __call__(self):
         t1 = timer()
         self.result['data'] = []
         self.result['label'] = []
 
-        if self._cur + self.imgs_per_batch >= len(self.imlist):
+        if self._cur + self.params['imgs_per_batch'] >= len(self.imlist):
             self._cur = 0
             shuffle(self.imlist)
         
         # Grab images names from imlist
-        imnames = self.imlist[self._cur : self._cur + self.imgs_per_batch]
+        imnames = self.imlist[self._cur : self._cur + self.params['imgs_per_batch']]
 
         # Figure out how many patches to grab from each image
-        patches_per_image = self.chunkify(self.batch_size, self.imgs_per_batch)
+        patches_per_image = self.chunkify(self.params['batch_size'], self.params['imgs_per_batch'])
 
         # Make nice output string
-        output_str = [str(npatches) + ' from ' + os.path.basename(imname) + '(id ' + str(itt) + ')' for imname, npatches, itt in zip(imnames, patches_per_image, range(self._cur, self._cur + self.imgs_per_batch))]
+        output_str = [str(npatches) + ' from ' + os.path.basename(imname) + '(id ' + str(itt) + ')' for imname, npatches, itt in zip(imnames, patches_per_image, range(self._cur, self._cur + self.params['imgs_per_batch']))]
         
         # Loop over each image
         for imname, npatches in zip(imnames, patches_per_image):
@@ -150,24 +130,27 @@ class PatchBatchAdvancer():
             # randomly select whether to flip this particular patch.
             flips = np.round(np.random.rand(npatches))*2-1
 
+            # get random offsets
+            rand_offsets = np.round(np.random.rand(npatches, 2) * (self.params['rand_offset'] * 2)  - self.params['rand_offset'])
+
             # Randomly permute the patch list for this image. Sampling is done with replacement 
             # so that if we ask for more patches than is available, it still computes.
-            (point_anns, height_cm) = self.imdict[os.path.basename(imname)]
+            (point_anns, height_cm) = self.imdict[os.path.basename(imname)] # read point annotations and image height in centimeters.
             point_anns = [point_anns[pp] for pp in np.random.choice(len(point_anns), size = npatches, replace = True)]
 
             # Load image
             im = np.asarray(Image.open(imname))
-            (im, scale) = coral_image_resize(im, self.scaling_method, self.scaling_factor, height_cm) #resize.
+            (im, scale) = coral_image_resize(im, self.params['scaling_method'], self.params['scaling_factor'], height_cm) #resize.
 
-            # Pad the boundaries                        
-            im = np.pad(im, ((self.crop_size*2, self.crop_size*2),(self.crop_size*2, self.crop_size*2), (0, 0)), mode='reflect')        
-            for ((row, col, label), angle, flip) in zip(point_anns, angles, flips):
+            # Pad the boundaries
+            crop_size = self.params['crop_size'] #for convenience, store this value locally                        
+            im = np.pad(im, ((crop_size * 2, crop_size * 2),(crop_size * 2, crop_size * 2), (0, 0)), mode='reflect')        
+            for ((row, col, label), angle, flip, rand_offset) in zip(point_anns, angles, flips, rand_offsets):
                 center_org = np.asarray([row, col])
-                center = np.round(self.crop_size*2 + center_org * scale).astype(np.int)
-                patch = self.transformer(crop_and_rotate(im, center, self.crop_size, angle, tile = False))
+                center = np.round(crop_size * 2 + center_org * scale + rand_offset).astype(np.int)
+                patch = self.transformer(crop_and_rotate(im, center, crop_size, angle, tile = False))
                 self.result['data'].append(patch[::flip, :, :])
                 self.result['label'].append(label)
-        # print 'TMPBA finished in {} seconds.'.format(timer() - t1)
 
     def chunkify(self, k, n):
         """ 
