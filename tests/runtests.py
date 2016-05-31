8e82d16ed64b2a6723dfc403bf33da0f2ef83ff8
Oscar Beijbom <oscar.beijbom@gmail.com>
2016-05-30 18:47:06 -0700

    removed runtests, not needed, use python -m unittest discover instead

--- tests/runtests.py
+++
@@ -1,9 +0,0 @@
-import test_misc
-import test_caffe
-import unittest
-
-suite = unittest.TestSuite([
-	unittest.TestLoader().loadTestsFromTestCase(test_caffe.TestCaffeTools),
-	unittest.TestLoader().loadTestsFromTestCase(test_misc.TestMiscTools) 
-	])
-unittest.TextTestRunner(verbosity=2).run(suite)
